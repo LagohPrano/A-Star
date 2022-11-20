3c4db8a9058e24730c1fbaba174a9ef6b310b269
LagohPrano <meisocialholic@gmail.com>
2022-11-20 15:19:48 -0800

    Changed export function

--- client/src/pages/RecommendationPage.js
+++ client/src/pages/RecommendationPage.js
@@ -1,2 +1,16 @@
 import React from 'react';
+import { getCompanyCategoryRecommendation, getCompanyRegionRecommendation } from '../fetcher';
 
+const recommendationColumns = [
+    {
+        title: 'Company Category',
+        dataIndex: 'Company Category',
+        key: 'Company Category',
+    },
+
+    {
+        title: 'Company Region',
+        dataIndex: 'Company Region',
+        key: 'Company Region',
+    }
+]
