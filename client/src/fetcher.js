53a62328feda06b4f1b8732337d89182fb62f2d6
LagohPrano <meisocialholic@gmail.com>
2022-11-20 14:21:24 -0800

    Implemented fetcher.js to return recommendation.

---
+++ client/src/fetcher.js
@@ -0,0 +1,15 @@
+import config from './config.json'
+
+const getCompanyCategoryRecommendation = async(major) => {
+    var res = await fetch(`http"//${config.server_host}:${config.server_port}/recommendation/company-category?major=${major}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getCompanyRegionRecommendation = async(major) => {
+    var res = await fetch(`http"//${config.server_host}:${config.server_port}/recommendation/company-region?major=${major}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
