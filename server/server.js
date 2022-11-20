805a7a39afb8e4966163869f004d9bdfdd4dc1bb
LagohPrano <meisocialholic@gmail.com>
2022-11-20 13:56:55 -0800

    Implemented two recommendation routes.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 const express = require("express")
+const mysql = require('mysql')
 const app = express()
 const cors = require("cors")
 
 app.use(cors())
 
+const routes = require('./routes')
+const config = require('./config.json')
+
 app.get("/", function(req, res) {
-    res.send({"name": "Jane Doe"}) // Should be json format
+    res.send({"name": "Mei"}) // Should be json format
 })
 
-/* Need to change it to your own database */
-app.listen(3000, () => {
-  console.log("app listening on port 3000")
-})
+// Route 
+app.get("/recommendation/company-category", routes.company_category_recommendations)
+app.get("/recommendation/company-region", routes.company_region_recommendations)
 
-  
+app.listen(config.server_port, () => {
+  console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
+});
+
+module.exports = app;
